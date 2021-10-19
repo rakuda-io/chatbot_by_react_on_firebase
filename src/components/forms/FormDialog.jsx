717a8ceec8d2e3bba436bde35dc6a9a17ba3e594
rakuda-io <rakudalms@gmail.com>
2021-10-19 20:56:13 +0900

    問い合わせフォーム完成

--- src/components/forms/FormDialog.jsx
+++ src/components/forms/FormDialog.jsx
@@ -3,14 +3,34 @@ import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import TextInput from './TextInput';
 
 export default class FormDialog extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      name: "",
+      email: "",
+      description: ""
+    }
+    this.inputName = this.inputName.bind(this);
+    this.inputEmail = this.inputEmail.bind(this);
+    this.inputDescription = this.inputDescription.bind(this);
   }
 
+  inputName = (e) => {
+    console.log(e.target.value)
+    this.setState({ name: e.target.value })
+  }
+
+  inputEmail = (e) => {
+    this.setState({ email: e.target.value })
+  }
+
+  inputDescription = (e) => {
+    this.setState({ description: e.target.value })
+  }
 
 
   render() {
@@ -22,20 +42,28 @@ export default class FormDialog extends React.Component {
       aria-describedby="alert-dialog-description"
     >
       <DialogTitle id="alert-dialog-title">
-        {"Use Google's location service?"}
+        お問い合わせフォーム
       </DialogTitle>
       <DialogContent>
-        <DialogContentText id="alert-dialog-description">
-          Let Google help apps determine location. This means sending anonymous
-          location data to Google, even when no apps are running.
-        </DialogContentText>
+        <TextInput
+          label={"名前(必須)"} multiline={false} rows={1}
+          value={this.state.name} type={"text"} onChange={this.inputName}
+        />
+        <TextInput
+          label={"メールアドレス(必須)"} multiline={false} rows={1}
+          value={this.state.email} type={"email"} onChange={this.inputEmail}
+        />
+        <TextInput
+          label={"お問い合わせ内容(必須)"} multiline={true} rows={5}
+          value={this.state.description} type={"text"} onChange={this.inputDescription}
+        />
       </DialogContent>
       <DialogActions>
         <Button onClick={this.props.handleClose}>
-          Disagree
+          キャンセル
         </Button>
-        <Button onClick={this.props.handleClose} autoFocus>
-          Agree
+        <Button onClick={this.submitForm} autoFocus>
+          送信する
         </Button>
       </DialogActions>
     </Dialog>
