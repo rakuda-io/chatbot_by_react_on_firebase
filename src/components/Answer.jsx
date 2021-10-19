a5f306b1efa1854afa1aa455ff119977b42dba4a
rakuda-io <rakudalms@gmail.com>
2021-10-19 17:06:22 +0900

    material-uiスタイル崩れバグ修正

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
-import Button from '@mui/material/Button';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    "button": {
+      borderColor: '#FFB549',
+      color: '#FFB549',
+      fontWeight: 600,
+      marginBottom: '8px',
+      "&:hover": {
+        backgroundColor: '#FFB549',
+        color: '#FFF'
+      },
+    },
+  }),
+);
 
 const Answer = (props) => {
+  const classes = useStyles();
+
   return (
       <Button
-        variant="contained"
-        color="secondary"
+        className={classes.button}
+        variant="outlined"
         onClick={() => props.select(props.content, props.nextId)}
       >
         {props.content}
