11154d8af2ca90d8a4ba1841ab4c941e7a2a2fc1
rakuda-io <rakudalms@gmail.com>
2021-10-26 06:38:18 +0900

    Re:Validate email format & validate null check & webhookURL to const
    間違えたのでやり直し
    1.メールアドレスの書式をちゃんとした書式か検証し弾くように修正しました。
    2.入力欄に空白（未入力）を検証し弾くように修正しました。
    3.webhookURLがgithub上に共有されないように別ファイルに定数化しました。

--- src/components/forms/FormDialog.jsx
+++ src/components/forms/FormDialog.jsx
@@ -5,6 +5,7 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import TextInput from './TextInput';
+import { WEBHOOK_URL } from '../../webhookConfig';
 
 const FormDialog = (props) => {
   const [name, setName] = useState("")
@@ -23,7 +24,34 @@ const FormDialog = (props) => {
     setDescription(e.target.value)
   },[setDescription])
 
+  //メールアドレスの書式をチェックするバリデーション
+  const validateEmailFormat = (email) => {
+    const regex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    return regex.test(email)
+  }
+
+  //入力欄の空欄をチェックするバリデーション
+  const validateRequiredInput = (...args) => {
+    let isBlank = false;
+    for (let i = 0; i < args.length; i=(i+1)|0) {
+        if (args[i] === "") {
+            isBlank = true;
+        }
+    }
+    return isBlank
+  };
+
   const submitForm = () => {
+    const isBlank = validateRequiredInput(name, email, description)
+    const isValidEmail = validateEmailFormat(email)
+
+    if (isBlank) {
+        alert('必須入力欄が空白です。')
+        return false
+    } else if (!isValidEmail) {
+        alert('メールアドレスの書式が異なります。')
+        return false
+    } else {
     const payload = {
       text: 'お問い合わせがありました\n' +
             'お名前：' + name + '\n' +
@@ -31,20 +59,18 @@ const FormDialog = (props) => {
             '【問い合わせ内容】：\n' + description
     }
 
-    // Slack　Incoming Webhook　URL
-    const url = 'https://hooks.slack.com/services/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'
-
-    // Slackへ通知を送る
-    fetch(url, {
-      method: 'POST',
-      body: JSON.stringify(payload)
-    }).then(() => {
-      alert('送信が完了しました。追ってご連絡差し上げます。')
-      setName("")
-      setEmail("")
-      setDescription("")
-      return props.handleClose();
-    })
+      // Slackへ通知を送る
+      fetch(WEBHOOK_URL, {
+        method: 'POST',
+        body: JSON.stringify(payload)
+      }).then(() => {
+        alert('送信が完了しました。追ってご連絡差し上げます。')
+        setName("")
+        setEmail("")
+        setDescription("")
+        return props.handleClose();
+      })
+    }
   }
 
   return(
